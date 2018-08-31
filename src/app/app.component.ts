6c0d0cc2fca2bec172924a32e700e35409f5ac42
MCreationz <biz.mastercreationz@gmail.com>
2018-08-31 15:13:32 -0700

    tourism code src folder added

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+   providers: [
+    { provide: CarouselConfig, useValue: { interval: 5000, noPause: true, showIndicators: true } }
+  ]
+})
+export class AppComponent {
+  title = 'Toutrism';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routerConfig';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    CarouselModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    AngularFontAwesomeModule,
+    MatMenuModule,
+    MatInputModule,
+    RouterModule.forRoot(appRoutes),
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
